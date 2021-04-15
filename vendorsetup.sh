7a337eb1f7d1348425e3f523301e39b9414ced27
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-16 01:29:23 +0530

    miatoll: arrow -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-12.0 kernel/xiaomi/sm6250
+git clone git@github.com:Xtended-Devices/device_xiaomi_sm6250-common.git -b xs device/xiaomi/sm6250-common
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-12.0 vendor/xiaomi/miatoll
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-12.0 vendor/xiaomi/sm6250-common
